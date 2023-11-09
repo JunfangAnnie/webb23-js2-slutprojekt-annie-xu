f970471a6aef47d982540733cc2e8616d7f1f82a
JunfangAnnie <125252462+JunfangAnnie@users.noreply.github.com>
2023-11-09 18:36:35 +0100

    uppdatera lagersaldo

--- webshop-slutprojekt/src/App.jsx
+++ webshop-slutprojekt/src/App.jsx
@@ -1,69 +1,79 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Navigation from './components/Navigation';
 import Nav from './components/Nav';
 import { BrowserRouter as Router } from 'react-router-dom';
-import "./css/Style.css";
+import './css/Style.css';
 
 const App = () => {
-  const [productItems] = useState([
+  const initialProductItems = [
     {
-      id: "1",
-      name: "TIAKI Madagascar",
+      id: '1',
+      name: 'TIAKI Madagascar',
       price: 165,
-      image: "./img/tiaki.jpg",
+      image: './img/tiaki.jpg',
       stock: 10,
-  },
-  {
-      id: "2",
-      name: "TIAKI Fruit",
+    },
+    {
+      id: '2',
+      name: 'TIAKI Fruit',
       price: 129,
-      image: "./img/tiaki-fruit.jpg",
+      image: './img/tiaki-fruit.jpg',
       stock: 5,
-  },
-  {
-      id: "3",
-      name: "Kattspö 3 i 1",
+    },
+    {
+      id: '3',
+      name: 'Kattspö 3 i 1',
       price: 29,
-      image: "./img/kattspo.jpg",
+      image: './img/kattspo.jpg',
       stock: 10,
-  },
-  {
-      id: "4",
-      name: "KONG Kickeroo",
+    },
+    {
+      id: '4',
+      name: 'KONG Kickeroo',
       price: 75,
-      image: "./img/kong.jpg",
+      image: './img/kong.jpg',
       stock: 7,
-  },
-  {
-      id: "5",
-      name: "TRIXIE Plyschmöss",
+    },
+    {
+      id: '5',
+      name: 'TRIXIE Plyschmöss',
       price: 39,
-      image: "./img/trixie.jpg",
+      image: './img/trixie.jpg',
       stock: 25,
-  },
-  ]);
+    },
+  ];
 
+  const [productItems, setProductItems] = useState(initialProductItems);
   const [shoppingCartItems, setShoppingCartItems] = useState([]);
 
   const handleBuyClick = (product) => {
     const updatedShoppingCartItems = [...shoppingCartItems];
+    const updatedProductItems = [...productItems];
+
     const existingProduct = updatedShoppingCartItems.find((item) => item.id === product.id);
-  
-    if (existingProduct) {
-      if (existingProduct.quantity < product.stock) {
-        existingProduct.quantity += 1;
-        setShoppingCartItems(updatedShoppingCartItems);
-      } else {
-        alert("Denna produkt är tyvärr slutsåld.");
-      }
+    const productIndex = updatedProductItems.findIndex((item) => item.id === product.id);
+
+    if (existingProduct && updatedProductItems[productIndex].stock > 0) {
+      existingProduct.quantity += 1;
+      updatedProductItems[productIndex].stock -= 1;
+    } else if (!existingProduct && updatedProductItems[productIndex].stock > 0) {
+      updatedShoppingCartItems.push({ ...product, quantity: 1 });
+      updatedProductItems[productIndex].stock -= 1;
     } else {
-      if (product.stock > 0) {
-        updatedShoppingCartItems.push({ ...product, quantity: 1 });
-        setShoppingCartItems(updatedShoppingCartItems);}
+      alert('Denna produkt är tyvärr slutsåld.');
     }
+
+    setShoppingCartItems(updatedShoppingCartItems);
+    setProductItems(updatedProductItems);
   };
-  
-  return ( 
+
+  const handleEmptyCart = () => {
+    console.log("Test")
+    setProductItems(initialProductItems)
+    setShoppingCartItems([]);
+  };
+
+  return (
     <div>
       <Router>
         <Navigation />
@@ -72,11 +82,11 @@ const App = () => {
           shoppingCartItems={shoppingCartItems}
           handleBuyClick={handleBuyClick}
           setShoppingCartItems={setShoppingCartItems}
-        />        
+          handleEmptyCart={handleEmptyCart}
+        />
       </Router>
     </div>
   );
 };
-
 
 export default App;
